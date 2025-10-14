92d30054f045e1394f9c2c7ff171bc7fea95cb16
larads <dasilval.mariana@gmail.com>
2025-10-14 13:39:10 -0400

    feat: add environment configuration and update database connection to use SUPABASE_URL

--- src/drizzle/client.ts
+++ src/drizzle/client.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import { env } from "../env";
 import { sensorBME680 } from "./schema/sensorBME680";
 
-export const pg = postgres(env.POSTGRES_URL)
+export const pg = postgres(env.SUPABASE_URL)
 export const db = drizzle(pg, {
     schema: {
         sensorBME680
